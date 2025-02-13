ccef806ba4c59e6f1057491b980156f46196cada
MateusNespoli <mateusnespolii@gmail.com>
2025-02-12 21:14:55 -0300

    state lift

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -7,6 +7,11 @@ import ConditionalRender from './components/ConditionalRender';
 import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
 import Fragment  from './components/Fragment';
+import Container from './components/Container';
+import ExecuteFunction from './components/ExecuteFunction';
+import { useState } from 'react';
+import Message from './components/Message';
+import ChangeMessageState from './components/ChangeMessageState';
 
 function App() {
 
@@ -15,6 +20,16 @@ function App() {
           {id: 2, brand: "KIA", color:"roxa", newCar: false, km:5000},
           {id: 3, brand: "Toyota", color:"branca", newCar: false, km:900000}
         ]
+    
+    function showMessage () {
+      console.log("Evento do componente pai")
+    }
+
+    const [message, setMessage] = useState("");
+    
+    const handleMessage = (msg) => {
+      setMessage(msg);
+    };
 
   return(
 
@@ -43,6 +58,7 @@ function App() {
       {/* Loop em array de objetos */}
       {cars.map((car) =>
         <CarDetails 
+        key={car.id}
         brand={car.brand}
         color={car.color}
         km={car.km}
@@ -50,6 +66,16 @@ function App() {
       )}
       {/* Frangment */}
       <Fragment propFragment="teste"/>
+      {/* Children */}
+      <Container myValue="teste">
+        <p>E este é o conteúdo</p>
+        <p>O valor é: </p>
+      </Container>
+      {/* executar função */}
+      <ExecuteFunction myFunction= {showMessage} />
+      {/* State lift */}
+      <Message msg={message} />
+      <ChangeMessageState handleMessage={handleMessage}/>
     </div>
    
   );
